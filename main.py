a3b7f996b59aad5ea9bacd2cdc23d3cd0b117bd7
w0nkest <nmdvoret@gmail.com>
2022-12-26 22:17:07 +0900

    типо меню начал

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
 
 
+def textname(screen):
+    screen.fill((0, 0, 0))
+    text = pygame.font.Font(None, 50).render("a game", True, (255, 255, 255))
+    text_x, text_y = width // 2 - text.get_width() // 2, 80
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 0, 255), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
+    text = pygame.font.Font(None, 15).render("yeah, just a game (there soon will be buttons, nevermind)<3",
+                                             True, (255, 255, 255))
+    text_x, text_y = width // 2 - text.get_width() // 2, 130
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+
+
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 700, 700
+    size = width, height = 700, 400
     screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
 
     running = True
     while running:
@@ -12,5 +30,7 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
+        textname(screen)
+        clock.tick(10)
         pygame.display.flip()
     pygame.quit()
